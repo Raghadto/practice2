4224292392de68accec68b6a992b14a052949f1e
Raghadto <raghadaltorefi@gmail.com>
2022-02-02 11:59:10 +0300

    Initial Commit

---
+++ practice2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  practice2
+//
+//  Created by raghad Mohammed on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ practice2/practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  practice2App.swift
+//  practice2
+//
+//  Created by raghad Mohammed on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
