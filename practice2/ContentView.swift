f87b430acbc48c32c8d4350622da295f13826cab
Raghadto <raghadaltorefi@gmail.com>
2022-02-02 12:01:08 +0300

    commit 1

--- practice2/ContentView.swift
+++ practice2/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+        Text("Hello, world!")
+            .padding()
+        Text("Hello, world!")
+            .padding()
+        
     }
 }
 
